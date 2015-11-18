ef7ba31befa30be5fe7999d557f25f428d5653b1
Ryan Lober <rlober@gmail.com>
2015-11-18 11:13:30 +0100

    experimentalTraj changes

--- wocra/include/wocra/Trajectory/wOcraExperimentalTrajectory.h
+++ wocra/include/wocra/Trajectory/wOcraExperimentalTrajectory.h
@@ -10,25 +10,65 @@ namespace wocra
 
 class wOcraExperimentalTrajectory : public wocra::wOcraTrajectory {
     public:
-        // wOcraExperimentalTrajectory(Eigen::MatrixXd& waypoints, bool endsWithQuaternion = false);
-        // wOcraExperimentalTrajectory(const Eigen::VectorXd& startingVector, const Eigen::VectorXd& endingVector, bool endsWithQuaternion = false);
-        // wOcraExperimentalTrajectory(Eigen::Displacementd& startingDisplacement, Eigen::Displacementd& endingDisplacement, bool endsWithQuaternion = true);
-        // wOcraExperimentalTrajectory(Eigen::Rotation3d& startingOrientation, Eigen::Rotation3d& endingOrientation, bool endsWithQuaternion = true);
 
-
-
+        void calculateVarianceParameters();
 
         Eigen::MatrixXd getDesiredValues(double time);
+        Eigen::VectorXd getVariance(double time);
+
+
+        void getDesiredValues(double time, Eigen::MatrixXd& desiredValues, Eigen::VectorXd& variance);
         void getDesiredValues(double time, Eigen::Displacementd& position, Eigen::Twistd& velocity, Eigen::Twistd& acceleration);
 
+        double getMaxVariance();
 
+        Eigen::MatrixXd getWaypointData();
 
+        void addNewWaypoint(Eigen::VectorXd newWaypoint, double waypointTime);
+        void removeYoungestWaypoint();
+        void removeWaypoint(int index);
+
+        Eigen::MatrixXd getRbfnKernelCurves();
 
     protected:
+        virtual void initializeTrajectory();
+        double t0, t0_variance;
+        bool varianceStartTrigger;
 
-        double t0;
+    private:
+        Eigen::MatrixXd originalWaypoints;
+        int youngestWaypointIndex;
 
 
+        int numberOfKernels;
+        Eigen::VectorXd kernelCenters;  // where the centers are placed
+        Eigen::VectorXd maxCovariance;  // maximum allowable covariance (or variance for each dof)
+        double kernelLengthParameter;   // influence of the kernel centers on one another
+
+        Eigen::MatrixXd designMatrix, designMatrixInv;   // big K matrix
+
+
+        void precalculateTrajectory(double DT, Eigen::MatrixXd& path, Eigen::VectorXd& timeline);
+        Eigen::MatrixXd kernelFunction(double m);
+        Eigen::MatrixXd kernelFunction(Eigen::VectorXd& evalVec);
+
+
+
+        // rbfn
+        Eigen::VectorXd rbfnKernelFunction(double m);
+        Eigen::MatrixXd rbfnKernelFunction(Eigen::VectorXd& evalVec);
+        void calculateRbfnWeights();
+        Eigen::MatrixXd rbfnWeights;
+
+        Eigen::VectorXd getRbfnOutput(double m);
+        Eigen::MatrixXd getRbfnOutput(Eigen::VectorXd& evalVec);
+
+        Eigen::VectorXd posOld;
+        Eigen::VectorXd velOld;
+        double t_old;
+
+
+        void reinitialize();
 
 };
 
