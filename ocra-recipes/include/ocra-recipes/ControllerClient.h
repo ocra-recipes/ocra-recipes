6419c545aae68379e27853bb52c83968f9708563
Ryan Lober <rlober@gmail.com>
2016-03-23 16:38:03 +0100

    Client inerits from RateThread

--- ocra-recipes/include/ocra-recipes/ControllerClient.h
+++ ocra-recipes/include/ocra-recipes/ControllerClient.h
@@ -6,20 +6,38 @@
 #include <ocra-recipes/RobotState.h>
 #include <yarp/os/Network.h>
 #include <yarp/os/Port.h>
+#include <yarp/os/RateThread.h>
 
 namespace ocra_recipes
 {
 
-class ControllerClient {
+class ControllerClient : public yarp::os::RateThread
+{
 
 public:
     ControllerClient ();
-    ControllerClient (std::shared_ptr<ocra::Model> derivedModelPtr);
+    ControllerClient (std::shared_ptr<ocra::Model> derivedModelPtr, const int loopPeriod = DEFAULT_LOOP_PERIOD);
     virtual ~ControllerClient ();
+
+    // RateThread virtual functions
+    virtual bool threadInit();
+    virtual void threadRelease();
+    virtual void run();
+
+    virtual void printHelp(){std::cout << "Hey I am the base ControllerClient class!" << std::endl;}
+
+    int getExpectedPeriod(){return expectedPeriod;}
+
+protected:
+    virtual bool initialize(){return true;}
+    virtual void release(){/* Do nothing. */}
+    virtual void loop() = 0;
 
 private:
     std::shared_ptr<ClientCommunications> clientComs;
     std::shared_ptr<ocra::Model> model;
+
+    bool isReady;
 
     yarp::os::Port statesPort;
     StateListener stateCallback;
@@ -28,6 +46,9 @@ private:
     int clientNumber;
 
     yarp::os::Network yarp;
+
+    int expectedPeriod;
+    static const int DEFAULT_LOOP_PERIOD = 10; // ms
 };
 
 
--- ocra-recipes/src/ControllerClient.cpp
+++ ocra-recipes/src/ControllerClient.cpp
@@ -5,12 +5,17 @@ using namespace ocra_recipes;
 int ControllerClient::CONTROLLER_CLIENT_COUNT = 0;
 
 ControllerClient::ControllerClient()
+: yarp::os::RateThread(DEFAULT_LOOP_PERIOD)
+, isReady(false)
+, expectedPeriod(DEFAULT_LOOP_PERIOD)
 {
 
 }
 
-ControllerClient::ControllerClient(std::shared_ptr<ocra::Model> derivedModelPtr)
-: model(derivedModelPtr)
+ControllerClient::ControllerClient(std::shared_ptr<ocra::Model> derivedModelPtr, const int loopPeriod)
+: yarp::os::RateThread(loopPeriod)
+, model(derivedModelPtr)
+, expectedPeriod(loopPeriod)
 {
     clientNumber = ++ControllerClient::CONTROLLER_CLIENT_COUNT;
 
@@ -26,9 +31,29 @@ ControllerClient::ControllerClient(std::shared_ptr<ocra::Model> derivedModelPtr)
     statesPort.setReader(stateCallback);
 
     yarp.connect("/ControllerClient/states:o", statesPort_Name.c_str());
+
+    isReady = true;
 }
 
 ControllerClient::~ControllerClient()
 {
 
+}
+
+bool ControllerClient::threadInit()
+{
+    if(isReady)
+        return initialize();
+    else
+        return false;
+}
+
+void ControllerClient::run()
+{
+    loop();
+}
+
+void ControllerClient::threadRelease()
+{
+    release();
 }
