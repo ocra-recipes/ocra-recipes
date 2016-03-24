07a58021bd7d8a318176db63f9263a84f61339ab
Ryan Lober <rlober@gmail.com>
2016-03-24 13:32:03 +0100

    Created ClientManager.

---
+++ ocra-recipes/include/ocra-recipes/ClientManager.h
@@ -0,0 +1,148 @@
+/*! \file       ClientManager.h
+ *  \brief      Module class for the controller Client.
+ *  \details
+ *  \author     [Ryan Lober](http://www.ryanlober.com)
+ *  \author     [Antoine Hoarau](http://ahoarau.github.io)
+ *  \date       Feb 2016
+ *  \copyright  GNU General Public License.
+ */
+/*
+ *  This file is part of ocra-icub.
+ *  Copyright (C) 2016 Institut des Systèmes Intelligents et de Robotique (ISIR)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLIENT_MANAGER_H
+#define CLIENT_MANAGER_H
+
+#include <iostream>
+#include <memory>
+
+#include <yarp/os/RFModule.h>
+#include <yarp/os/RpcServer.h>
+
+#include <yarpWholeBodyInterface/yarpWholeBodyInterface.h>
+#include "ocra-recipes/ControllerClient.h"
+// #include "ocra-icub/Utilities.h"
+
+namespace ocra_recipes
+{
+/*! \class ClientManager
+ *  \brief The controller module which launches the controller thread.
+ *
+ *  Basically all this does is parse the command line arguments and look for the various config and task set files. It then instantiates a WBI instance (yarpWBI specifically) and a \ref IcubControllerClient instance. It launches these threads and then basically just waits till it gets a kill (ctrl+c) command to close them down. Does a little keeping track of time as well.
+ */
+class ClientManager : public yarp::os::RFModule
+{
+public:
+    /*! Constructor which essentially does nothing.
+     */
+    ClientManager(std::shared_ptr<ControllerClient> customClient);
+
+    /*! Destructor which essentially does nothing.
+     */
+    ~ClientManager();
+
+    /*! Configures the module by parsing the RF contents. Passes to client when done.
+     *  \param rf A resource finder instance which is initialized from the command line args.
+     *
+     *  \return True or false if the configuration was successful.
+     */
+    bool configure(yarp::os::ResourceFinder &rf);
+
+    /*! Simply calls client->start() then runModule()
+     */
+    int launchClient();
+
+    /*! Interrupts the module execution and stops the control and wbi threads.
+     */
+    bool interruptModule();
+
+    /*! Closes the module. First shuts down the threads.
+     */
+    bool close();
+
+    /*! Updates the ClientManager. Basically just clocks the thread run() method.
+     *  \return Whether or not the clocking functions worked.
+     */
+    bool updateModule();
+
+    /*! Prints all the command line args one could use.
+     */
+    void printHelp();
+
+    /*! Gets the name of the module. Override this function to set a different name.
+     *  \return A string with the module name.
+     */
+    virtual std::string getManagerName();
+
+
+public:
+    /*! \class moduleCallback
+     *  \brief A callback function which binds the rpc server port opened in the contoller server module to the controller thread's parsing function.
+     */
+    class moduleCallback : public yarp::os::PortReader
+    {
+
+    public:
+
+        /*! Constructor
+         *  \param newModuleRef A shared pointer to the control thread.
+         */
+        moduleCallback(ClientManager& newModuleRef);
+
+        /*! read
+         *  \param connection Reads a port connection.
+         *
+         *  \return A boolean which tells whether or not a message was read.
+         */
+        virtual bool read(yarp::os::ConnectionReader& connection);
+
+    private:
+        ClientManager& moduleRef; /*!< A ref to the client module. */
+    };
+
+
+private:
+    void callbackParser(yarp::os::Bottle& message, yarp::os::Bottle& reply);
+
+protected:
+    virtual void customCallbackParser(yarp::os::Bottle& message, yarp::os::Bottle& reply);
+    virtual bool customUpdateModule();
+
+
+private:
+    std::shared_ptr<ControllerClient> client;               /*!< The controller client thread pointer. */
+    std::shared_ptr<moduleCallback> rpcCallback;                 /*!< Rpc server port callback function. */
+    yarp::os::RpcServer rpcPort;                            /*!< Rpc server port. */
+    yarp::os::Log yLog;                                     /*!< For logging in yarp. */
+
+    int expectedClientPeriod;                         /*!< The expected period of the client thread. In ms.*/
+    double avgTime;                                         /*!< Average time between successive calls of the `run()` method.*/
+    double stdDev;                                          /*!< Standard deviation of the average time between successive calls of the `run()` method. */
+    double avgTimeUsed;                                     /*!< Average time for the `run()` method to execute. Should be close to avgTime. */
+    double stdDevUsed;                                      /*!< Standard deviation of the average time for the `run()` method to execute. */
+
+    std::string rpcPortName;                                /*!< The name of the rpc port for the thread. */
+
+    static int CONTROLLER_CLIENT_MANAGER_COUNT;              /*!< A count that is incremented each time a client thread is constructed. */
+    int moduleNumber;                                       /*!< The unique thread number. */
+
+
+};
+
+} /* ocra_recipes */
+
+#endif
---
+++ ocra-recipes/src/ClientManager.cpp
@@ -0,0 +1,156 @@
+/*! \file       ClientManager.h
+ *  \brief      Module class for the controller Client.
+ *  \details
+ *  \author     [Ryan Lober](http://www.ryanlober.com)
+ *  \author     [Antoine Hoarau](http://ahoarau.github.io)
+ *  \date       Feb 2016
+ *  \copyright  GNU General Public License.
+ */
+/*
+ *  This file is part of ocra-icub.
+ *  Copyright (C) 2016 Institut des Systèmes Intelligents et de Robotique (ISIR)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ocra-recipes/ClientManager.h"
+
+using namespace ocra_recipes;
+
+int ClientManager::CONTROLLER_CLIENT_MANAGER_COUNT = 0;
+
+ClientManager::ClientManager(std::shared_ptr<ControllerClient> customClient)
+{
+    // Increment the module counter and save it in module number.
+    moduleNumber = ++ClientManager::CONTROLLER_CLIENT_MANAGER_COUNT;
+
+    client = customClient;
+    expectedClientPeriod = client->getExpectedPeriod();
+}
+
+ClientManager::~ClientManager()
+{
+    rpcPort.close();
+}
+
+std::string ClientManager::getManagerName()
+{
+    return "ControllerClientManager_"+std::to_string(moduleNumber);
+}
+
+bool ClientManager::configure(yarp::os::ResourceFinder &rf)
+{
+    return client->configure(rf);
+}
+
+int ClientManager::launchClient()
+{
+    client->start();
+    return runModule();
+}
+
+bool ClientManager::interruptModule()
+{
+    if(client)
+        client->suspend();
+    return true;
+}
+
+bool ClientManager::close()
+{
+    /* Stop the control thread. */
+    if(client){
+        client->stop();
+    }
+
+    /* Print performance information */
+    printf("[PERFORMANCE INFORMATION]:\n");
+    printf("Expected period %d ms.\nReal period: %3.1f+/-%3.1f ms.\n", expectedClientPeriod, avgTime, stdDev);
+    printf("Real duration of 'run' method: %3.1f+/-%3.1f ms.\n", avgTimeUsed, stdDevUsed);
+    if(avgTime<0.5*(double)expectedClientPeriod)
+        printf("Next time you could set a lower period to improve the controller performance.\n");
+    else if(avgTime>1.3*(double)expectedClientPeriod)
+        printf("The period you set was impossible to attain. Next time you could set a higher period.\n");
+    return true;
+}
+
+bool ClientManager::updateModule()
+{
+    // Get the average time between two calls of the Rate.run() method.
+    client->getEstPeriod(avgTime, stdDev);
+
+    // Get the average time the .run() method takes to compute the control.
+    client->getEstUsed(avgTimeUsed, stdDevUsed);
+
+    // If the period of the control thread is too slow then print a warning.
+    if(avgTime > 1.3*(double)expectedClientPeriod)
+    {
+        yLog.warning() << "CLIENT THREAD LOOP IS TOO SLOW\nReal period: "<< avgTime <<"+/-"<< stdDev <<"\nExpected period: " << expectedClientPeriod <<"\nDuration of 'run' method: "<<avgTimeUsed<<"+/-"<< stdDevUsed<<"\n";
+    }
+
+    return customUpdateModule();
+}
+
+void ClientManager::printHelp()
+{
+    std::cout << "Hey there this is the help for the ClientManager. Here is what the client is saying..." << std::endl;
+    client->printHelp();
+}
+
+void ClientManager::callbackParser(yarp::os::Bottle& message, yarp::os::Bottle& reply)
+{
+    if (message.size() != 0) {
+        reply.clear();
+        customCallbackParser(message, reply);
+    }
+}
+
+void ClientManager::customCallbackParser(yarp::os::Bottle& message, yarp::os::Bottle& reply)
+{
+    // Do nothing if not implemented.
+}
+bool ClientManager::customUpdateModule()
+{
+    // Do nothing if not implemented.
+    return true;
+}
+
+
+/**************************************************************************************************
+                                    Nested moduleCallback Class
+**************************************************************************************************/
+ClientManager::moduleCallback::moduleCallback(ClientManager& newModuleRef)
+: moduleRef(newModuleRef)
+{
+    //do nothing
+}
+
+bool ClientManager::moduleCallback::read(yarp::os::ConnectionReader& connection)
+{
+    yarp::os::Bottle input, reply;
+
+    if (!input.read(connection)){
+        return false;
+    }
+    else{
+        moduleRef.callbackParser(input, reply);
+        yarp::os::ConnectionWriter* returnToSender = connection.getWriter();
+        if (returnToSender!=NULL) {
+            reply.write(*returnToSender);
+        }
+        return true;
+    }
+}
+/**************************************************************************************************
+**************************************************************************************************/
